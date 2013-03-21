ecfc713e49682879d4d3436fc0df70885c3529e4
oncletom <thomas@oncle-tom.net>
2013-03-21 20:02:34 +0000

    Better tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,17 +37,13 @@ module.exports = function(grunt) {
     }
   });
 
-  // These plugins provide necessary tasks.
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
-  //grunt.loadNpmTasks('grunt-contrib-jshint');
-  //grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-jquerymanifest');
 
-  // Default task.
   grunt.registerTask('default', ['qunit', 'jquerymanifest']);
-  grunt.registerTask('build', ['copy:src']);
+  grunt.registerTask('build', ['copy', 'qunit', 'jquerymanifest']);
+  grunt.registerTask('build-quick', ['copy:src']);
   grunt.registerTask('test', ['copy:tests', 'qunit']);
 
 };
